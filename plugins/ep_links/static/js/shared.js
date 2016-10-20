6c63358b819b256f4b364d5d1d0181946ab81b0b
Anton Frolovsky <antonfrolovsky@gmail.com>
2016-10-21 00:17:23 +0400

    New plugin: ep_links for padlink and ohter link

---
+++ plugins/ep_links/static/js/shared.js
@@ -0,0 +1,23 @@
+exports.collectContentPre = function(hook, context) {
+  var tname = context.tname;
+  var state = context.state;
+  var lineAttributes = state.lineAttributes
+  var tagIndex = tname;
+  var fonts = ['fontarial'];
+
+  if (fonts.indexOf(tname) !== -1){
+    context.cc.doAttrib(state, tname);
+  }
+};
+
+// never seems to be run
+exports.collectContentPost = function(hook, context) {
+  var tname = context.tname;
+  var state = context.state;
+  var lineAttributes = state.lineAttributes
+  var tagIndex = tname;
+
+  if (tagIndex >= 0){
+    delete lineAttributes['sub'];
+  }
+};
