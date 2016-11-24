2d91a1f1c0fb4e4a89c70581eb51a0ca1a33121e
gaer <gaer.nikita@gmail.com>
2016-11-24 17:58:16 +0400

    Request rate limiter and expiration of spammer IPs check

--- plugins/ep_open/hooks.js
+++ plugins/ep_open/hooks.js
@@ -13,13 +13,20 @@ const settings = require('ep_etherpad-lite/node/utils/Settings');
 const Honeypot = require('honeypot');
 const honeypot = new Honeypot('ycvyvsukawvu');
 const logger = require('ep_etherpad-lite/node_modules/log4js').getLogger('API');
+const RateLimit = require('express-rate-limit');
 
-const checkedIPs = [];
-const blacklistedIPs = [];
+const checkedIPs = {};
+const blacklistedIPs = {};
 
 exports.expressCreateServer = function(hookName, args) {
 	const { app } = args;
 
+	app.use(new RateLimit({
+		windowMs: 10 * 60 * 1000, // 10 minutes
+		max: 10000, // limit each IP to 10000 requests per windowMs
+		delayMs: 0, // disable delaying - full speed until the max limit is reached
+		message: "Too many requests from this IP, please try again after 10 minutes"
+	}));
 	// Check IP address in blacklist of spammers
 	app.use(function(request, response, next) {
 		const ip = (
@@ -28,19 +35,23 @@ exports.expressCreateServer = function(hookName, args) {
 			request.connection.remoteAddress ||
 			request.socket.remoteAddress
 		);
+		const ts = new Date().getTime();
+		const expiratedTs = ts - 24 * 60 * 60 * 1000; // Check expiration is 24 hours
 
-		if (blacklistedIPs.indexOf(ip) !== -1) {
+		if (blacklistedIPs[ip] && blacklistedIPs[ip] > expiratedTs) {
 			return response.status(403).send('Access denied! Your IP address is on the blacklist of suspicious and malicious IPs.');
+		} else {
+			delete blacklistedIPs[ip];
 		}
 
-		if (checkedIPs.indexOf(ip) === -1) {
-			checkedIPs.push(ip);
+		if (!checkedIPs[ip] || checkedIPs[ip] < expiratedTs) {
+			checkedIPs[ip] = ts;
 			honeypot.query(ip, function(error, response) {
 				const type = response ? response[0].split('.')[3] : 0;
 
 				// If type of user is harvester or comment spammer, then block it
 				if (type > 1) {
-					blacklistedIPs.push(ip);
+					blacklistedIPs[ip] = ts;
 					logger.warn('Blacklist check | blocked IP: ' + ip, honeypot.getFormattedResponse());
 				} else {
 					logger.info('Blacklist check | allowed IP: ' + ip);
